78d021e03c9f79ef943e3fd5ae85ad22f7d04710
Chad Cook <Charles.Cook28@gmail.com>
2021-03-20 17:57:53 -0500

    got the post and delete functions to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,51 @@
 
-
-var noteData = require("../db/db.json");
+const fs = require('fs');
 
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
-        res.json(noteData);
+        fs.readFile('db/db.json', 'utf8', (error, data) =>
+        {
+            if (error) console.log(error);
+            console.log(data);
+            data = JSON.parse(data);
+            console.log(data);
+            res.json(data);
+        }
+        );
+        
     });
 
     app.post("/api/notes", function(req, res) {
-        tableData.push(req.body);
+        fs.readFile('db/db.json', 'utf8', (error, data) =>
+        {
+            if (error) console.log(error);
+            data = JSON.parse(data);
+            req.body.id= Date.now();
+            data.push(req.body)
+            fs.writeFile('db/db.json', JSON.stringify(data), (err) => {            
+            err ? console.error(err) : console.log('Success!')
+            res.json(data)
+            }
+            );
+        }
+        );
+        
     });
+
+    app.delete("/api/notes/:id", function(req, res) {
+        var id = req.params.id;
+        fs.readFile("db/db.json", "utf8", (error, data) =>
+        {
+            if (error) console.log(error);
+            data = JSON.parse(data);
+            date = data.filter(notes => notes.id != id)
+            fs.writeFile("db/db.json", JSON.stringify(date), (err) => {            
+            err ? console.error(err) : console.log("Success!")
+            res.json(data)
+            })
+        })
+    })
 
     app.post("/api/clear", function(req, res) {
         // Empty out the arrays of data
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3003;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
 
@@ -22,41 +23,9 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.get("/note", function(req, res) {
-  res.json(dbJSON);
-});
-
-app.post("/note", function(req, res) {
-  // Validate request body
-  if(!req.body.title) {
-    return res.json({error: "Missing required title"});
-  }
-
-  // Copy request body and generate ID
-  const note = {...req.body, id: uuidv4()}
-
-  // Push note to dbJSON array - saves data in memory
-  dbJSON.push(note);
-
-  // Saves data to file by persisting in memory variable dbJSON to db.json file.
-  // This is needed because when we turn off server we loose all memory data like pbJSON variable.
-  // Saving to file allows us to read previous notes (before server was shutdown) from file.
-  fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(dbJSON), (err) => {
-    if (err) {
-      return res.json({error: "Error writing to file"});
-    }
-
-    return res.json(note);
-  });
-});
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 
 // Starts the server to begin listening
 // =============================================================
